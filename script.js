0de9128501ae2a10263ce9add69910e9215a77ff
havie236 <149625092+havie236@users.noreply.github.com>
2026-03-20 03:40:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -125,15 +125,7 @@ function startBlock() {
 // --- TASK LOGIC ---
 function generateMatrix() {
     const container = document.getElementById('matrix-container');
-    container.innerHTML = '';
-    currentTargetCount = 0; 
-    matrixTabSwitches = 0; 
-    matrixSwitchHistory = []; 
-
-    const gridSize = 8;
-    const totalCells = gridSize * gridSize;
-    let cellWidth = '40px';
-    let cellHeight = '40px';
+    container.innerHTML = ''; currentTargetCount = 0; matrixTabSwitches = 0; matrixSwitchHistory = [];
 
     for (let i = 0; i < 64; i++) {
         let isT = Math.random() > 0.5;
